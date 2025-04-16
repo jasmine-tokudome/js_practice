8d7e97f7cc1dab4623a86653c8a69d917743808b
Jasmine <103996128+jasmine-tokudome@users.noreply.github.com>
2025-04-16 21:15:15 +0900

    メッセージを表示できるようにする

--- omikuji.js
+++ omikuji.js
@@ -1,10 +1,10 @@
-function omikuji(){
+function omikuji(year){
     let hako = ["大吉","中吉","吉","小吉","凶","大凶"];
     let index = Math.floor(Math.random() * 6);
     let unsei = hako[index];
-    console.log(unsei);
+    console.log(year + "今年の運勢は…" + unsei + "です！");
 }
 
 for (let i = 0;i<10;i++){
-    omikuji();
+    omikuji(2025);
 }
