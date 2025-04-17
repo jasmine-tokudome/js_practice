ad0916c69906c050d3d84976c7153d56459b29ea
Jasmine <103996128+jasmine-tokudome@users.noreply.github.com>
2025-04-17 21:24:58 +0900

    大吉の時だけ運勢が表示される

--- omikuji.js
+++ omikuji.js
@@ -2,7 +2,9 @@ function omikuji(year){
     let hako = ["大吉","中吉","吉","小吉","凶","大凶"];
     let index = Math.floor(Math.random() * 6);
     let unsei = hako[index];
-    console.log(year + "今年の運勢は…" + unsei + "です！");
+    if(index == 0){
+        console.log(year + "今年の運勢は…" + unsei + "です！");
+    }
 }
 
 for (let i = 2025;i < 2125;i++){
